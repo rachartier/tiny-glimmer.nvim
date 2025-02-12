92f7d80f890f62213e4b5326e2370a8ddb3abf4b
Raphaël Chartier <raphaelchartier@free.fr>
2025-02-12 22:47:12 +0100

    fix: correct key combination handling in hijack function

--- lua/tiny-glimmer/overwrite/search.lua
+++ lua/tiny-glimmer/overwrite/search.lua
@@ -1,11 +1,7 @@
 local M = {}
 
-local function search(opts, keys, search_pattern)
+local function search(opts, search_pattern)
 	local buf = vim.api.nvim_get_current_buf()
-
-	if keys ~= nil then
-		vim.fn.feedkeys(vim.v.count1 .. keys, "n")
-	end
 
 	vim.schedule(function()
 		local cursor_pos = vim.api.nvim_win_get_cursor(0)
@@ -22,45 +18,31 @@ local function search(opts, keys, search_pattern)
 			end_col = cursor_pos[2] + #matches[1].text,
 		}
 
-		require("tiny-glimmer.animation.factory").get_instance():create_text_animation(opts.default_animation, {
-			base = {
-				range = range,
-			},
-		})
+		require("tiny-glimmer.animation.factory")
+			.get_instance()
+			:create_named_text_animation("search", opts.default_animation, {
+				base = {
+					range = range,
+				},
+			})
 	end)
 end
 
 function M.search_on_line(opts)
-	search(opts, nil, vim.fn.getreg("/"))
+	search(opts, vim.fn.getreg("/"))
 end
 
 function M.search_next(opts)
-	local keys
-
-	if type(opts.next_mapping) == "function" then
-		keys = opts.next_mapping()
-	else
-		keys = opts.next_mapping
-	end
-
-	search(opts, keys, vim.fn.getreg("/"))
+	search(opts, vim.fn.getreg("/"))
 end
 
 function M.search_prev(opts)
-	local keys
-
-	if type(opts.prev_mapping) == "function" then
-		keys = opts.prev_mapping()
-	else
-		keys = opts.prev_mapping
-	end
-
-	search(opts, keys, vim.fn.getreg("/"))
+	search(opts, vim.fn.getreg("/"))
 end
 
 function M.search_under_cursor(opts)
 	local word_under_cursor = vim.fn.expand("<cword>")
-	search(opts, "*", word_under_cursor)
+	search(opts, word_under_cursor)
 end
 
 return M

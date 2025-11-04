b2a10e1821ac84d2856c361d054654c5fae48bd3
Raphaël CHARTIER <raphael.chartier@michelin.com>
2025-11-04 11:52:33 +0100

    style(examples): stylua

--- examples/01_basic_usage.lua
+++ examples/01_basic_usage.lua
@@ -2,32 +2,32 @@ local glimmer = require("tiny-glimmer.lib")
 
 -- Animate current line
 vim.keymap.set("n", "<leader>al", function()
-	glimmer.cursor_line("pulse")
+  glimmer.cursor_line("pulse")
 end)
 
 -- Animate cursor character
 vim.keymap.set("n", "<leader>ac", function()
-	glimmer.animate_range("fade", glimmer.get_cursor_range())
+  glimmer.animate_range("fade", glimmer.get_cursor_range())
 end)
 
 -- Animate visual selection
 vim.keymap.set("v", "<leader>av", function()
-	glimmer.visual_selection("fade")
+  glimmer.visual_selection("fade")
 end)
 
 -- Different effects
 vim.keymap.set("n", "<leader>a1", function()
-	glimmer.cursor_line("fade")
+  glimmer.cursor_line("fade")
 end)
 
 vim.keymap.set("n", "<leader>a2", function()
-	glimmer.cursor_line("pulse")
+  glimmer.cursor_line("pulse")
 end)
 
 vim.keymap.set("n", "<leader>a3", function()
-	glimmer.cursor_line("bounce")
+  glimmer.cursor_line("bounce")
 end)
 
 vim.keymap.set("n", "<leader>a4", function()
-	glimmer.cursor_line("rainbow")
+  glimmer.cursor_line("rainbow")
 end)
--- examples/02_autocmd_integration.lua
+++ examples/02_autocmd_integration.lua
@@ -2,47 +2,47 @@ local glimmer = require("tiny-glimmer.lib")
 
 -- Highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		if vim.v.event.operator == "y" then
-			local range = glimmer.get_yank_range()
-			if range then
-				glimmer.animate_range("fade", range)
-			end
-		end
-	end,
+  callback = function()
+    if vim.v.event.operator == "y" then
+      local range = glimmer.get_yank_range()
+      if range then
+        glimmer.animate_range("fade", range)
+      end
+    end
+  end,
 })
 
 -- Pulsar effect on cursor movement
 local last_line = vim.api.nvim_win_get_cursor(0)[1]
 vim.api.nvim_create_autocmd("CursorMoved", {
-	callback = function()
-		local current_line = vim.api.nvim_win_get_cursor(0)[1]
-		if current_line ~= last_line then
-			glimmer.cursor_line("fade")
-			last_line = current_line
-		end
-	end,
+  callback = function()
+    local current_line = vim.api.nvim_win_get_cursor(0)[1]
+    if current_line ~= last_line then
+      glimmer.cursor_line("fade")
+      last_line = current_line
+    end
+  end,
 })
 
 -- Animate when entering insert mode
 vim.api.nvim_create_autocmd("InsertEnter", {
-	callback = function()
-		glimmer.cursor_line({ name = "fade", settings = { from_color = "DiffAdd" } })
-	end,
+  callback = function()
+    glimmer.cursor_line({ name = "fade", settings = { from_color = "DiffAdd" } })
+  end,
 })
 
 -- Animate diagnostic updates
 vim.api.nvim_create_autocmd("DiagnosticChanged", {
-	callback = function()
-		local diagnostics = vim.diagnostic.get(0)
-		if #diagnostics > 0 then
-			local diag = diagnostics[1]
-			glimmer.animate_range("pulse", {
-				start_line = diag.lnum,
-				start_col = 0,
-				end_line = diag.lnum,
-				end_col = 100,
-			}, { from_color = "DiagnosticError" })
-		end
-	end,
+  callback = function()
+    local diagnostics = vim.diagnostic.get(0)
+    if #diagnostics > 0 then
+      local diag = diagnostics[1]
+      glimmer.animate_range("pulse", {
+        start_line = diag.lnum,
+        start_col = 0,
+        end_line = diag.lnum,
+        end_col = 100,
+      }, { from_color = "DiagnosticError" })
+    end
+  end,
 })
